70b1c8140241115e4dd1e6d83d4633d7e5232f34
chatelao <chatelao@93e2e7f1-d98a-4af9-bc02-d7c9e78ee626>
2008-10-06 21:15:56 +0000

    - Version changed to 3.2.3.
    git-svn-id: https://svn.code.sf.net/p/php-addressbook/code/trunk@27 93e2e7f1-d98a-4af9-bc02-d7c9e78ee626

--- include/dbconnect.php
+++ include/dbconnect.php
@@ -145,6 +145,6 @@ $month_from_where = "$base_from LEFT OUTER JOIN $month_lookup ON $table.bmonth =
 
 $group_from_where = "$table_groups WHERE group_name = '$group_name'";
 
-$version = '3.2.2';
+$version = '3.2.3';
 
 ?>
