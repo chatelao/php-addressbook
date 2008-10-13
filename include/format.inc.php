ac8b17c2a515c1a4163cfb82d9488b7e410bb671
chatelao <chatelao@93e2e7f1-d98a-4af9-bc02-d7c9e78ee626>
2008-10-13 19:14:06 +0000

    - v3.2.12 released.
    git-svn-id: https://svn.code.sf.net/p/php-addressbook/code/trunk@52 93e2e7f1-d98a-4af9-bc02-d7c9e78ee626

--- include/format.inc.php
+++ include/format.inc.php
@@ -1,4 +1,15 @@
 <html>
 <meta http-equiv="Content-Type" content="text/html" charset="ISO-8859-1">
 <head>
+<!--
 <link rel="stylesheet" href="style.css" type="text/css">
+  -->
+<style>
+<?php
+ //
+ // Include to stylesheet, to improve the first 50%
+ // of non-cached accesses to the pages.
+ //
+ include ("style.css");
+ ?>
+</style>
