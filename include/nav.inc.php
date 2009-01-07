51fcf2bf5ef54c50a92d461b4fd37d0ba137cf29
chatelao <chatelao@93e2e7f1-d98a-4af9-bc02-d7c9e78ee626>
2009-01-07 08:08:46 +0000

    - Upd: Menu icons added.
    git-svn-id: https://svn.code.sf.net/p/php-addressbook/code/trunk@157 93e2e7f1-d98a-4af9-bc02-d7c9e78ee626

--- include/nav.inc.php
+++ include/nav.inc.php
@@ -1,42 +1,56 @@
 
 <table width="100" cellspacing="0" cellpadding="2">
   <tr>
-    <td><a href="./<?php echo ($group == "" || $group == "[none]" || $fixgroup ? "" : "?group=".$group); ?>"><?php echo msg('HOME'); ?></a></td>
-  </tr>
-  <tr>
-    <td><a href="birthdays<?php echo $page_ext; ?>"><?php echo msg('NEXT_BIRTHDAYS'); ?></a></td>
+    <td>
+    	<img src="<?php echo ${url_images}; ?>icons/font.png">
+    	<a href="./<?php echo ($group == "" || $group == "[none]" || $fixgroup ? "" : "?group=".$group); ?>"><?php echo msg('HOME'); ?></a></td>
   </tr>
   <tr><td/></tr>
 <?php if(! $read_only)
 { ?>
   <tr>
-    <td><a href="edit<?php echo $page_ext; ?>"><?php echo msg('ADD_NEW'); ?></a></td>
+    <td>
+    	<img src="<?php echo ${url_images}; ?>icons/add.png">
+    	<a href="edit<?php echo $page_ext; ?>"><?php echo msg('ADD_NEW'); ?></a></td>
   </tr>
 <?php 
-} ?>
-  <tr><td/></tr>
+} 
+if(!$read_only && $public_group_edit && $table_groups != "" && !$is_fix_group)
+{ ?>
+  <tr>
+    <td>
+      <img src="<?php echo ${url_images}; ?>icons/group.png">
+    	<a href="group<?php echo $page_ext; ?>"><?php echo msg('MANAGE_GROUPS'); ?></a></td>
+  </tr>
+<?php
+} ?>  <tr><td/></tr>
   <tr><td/></tr>
   <tr><td/></tr>
   <tr><td/></tr>
   <tr>
-    <td><a href="view<?php echo $page_ext_qry; ?>all&print"><?php echo msg('PRINT_ALL'); ?></a></td>
+    <td>
+    	<img src="<?php echo ${url_images}; ?>icons/date.png">
+    	<a href="birthdays<?php echo $page_ext; ?>"><?php echo msg('NEXT_BIRTHDAYS'); ?></a></td>
   </tr>
   <tr>
-    <td><a href="view<?php echo $page_ext_qry; ?>all&print&phones"><?php echo msg('PRINT_PHONES'); ?></a></td>
+    <td>
+    	<img src="<?php echo ${url_images}; ?>icons/printer.png">
+    	<a href="view<?php echo $page_ext_qry; ?>all&print"><?php echo msg('PRINT_ALL'); ?></a></td>
   </tr>
   <tr>
-    <td><a href="csv<?php echo $page_ext; ?>"><?php echo msg('EXPORT_CSV'); ?></a></td>
+    <td>
+      <img src="<?php echo ${url_images}; ?>icons/book_open.png">
+      <a href="view<?php echo $page_ext_qry; ?>all&print&phones"><?php echo msg('PRINT_PHONES'); ?></a></td>
+  </tr>
+  <tr>
+    <td>
+      <img src="<?php echo ${url_images}; ?>icons/page_excel.png">
+    	<a href="csv<?php echo $page_ext; ?>"><?php echo msg('EXPORT_CSV'); ?></a></td>
   </tr>                                                                   
   <tr><td/></tr>                                                          
   <tr><td/></tr>
   <tr><td/></tr>
-<?php if(!$read_only && $public_group_edit && $table_groups != "" && !$is_fix_group)
-{ ?>
-  <tr>
-    <td><a href="group<?php echo $page_ext; ?>"><?php echo msg('MANAGE_GROUPS'); ?></a></td>
-  </tr>
 <?php
-} 
 /*
  * Add some function only active on the 
  * "php-addressbook.sourceforge.net" Demopage.
