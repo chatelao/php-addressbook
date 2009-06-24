b93846862b92c936d1838a05d28cda98e9710dec
chatelao <chatelao@93e2e7f1-d98a-4af9-bc02-d7c9e78ee626>
2009-06-24 09:01:26 +0000

    - Remove unused images from "nav.inc.php".
    git-svn-id: https://svn.code.sf.net/p/php-addressbook/code/trunk@208 93e2e7f1-d98a-4af9-bc02-d7c9e78ee626

--- include/nav.inc.php
+++ include/nav.inc.php
@@ -1,32 +1,29 @@
 <ul>
 <li>
-	<img src="<?php echo $url_images; ?>icons/font.png" alt="home" />
 	<a href="./<?php echo ($group == "" || $group == "[none]" || $is_fix_group ? "" : "?group=".$group); ?>"><?php echo msg('HOME'); ?></a>
 </li>
 <?php if(! $read_only) { ?>
 <li>
-	<img src="<?php echo $url_images; ?>icons/add.png" alt="add new" /><a href="edit<?php echo $page_ext; ?>"><?php echo msg('ADD_NEW'); ?></a>
+	<a href="edit<?php echo $page_ext; ?>"><?php echo msg('ADD_NEW'); ?></a>
 </li>
 <?php 
 } 
 if(!$read_only && $public_group_edit && $table_groups != "" && !$is_fix_group)
 { ?>
-	<li><img src="<?php echo $url_images; ?>icons/group.png" alt="groups" /><a href="group<?php echo $page_ext; ?>"><?php echo msg('GROUPS'); ?></a></li>
+	<li>
+		<a href="group<?php echo $page_ext; ?>"><?php echo msg('GROUPS'); ?></a>
+	</li>
 <?php } ?>
 	<li>
-		<img src="<?php echo $url_images; ?>icons/date.png" alt="next birthdays" />
 		<a href="birthdays<?php echo $page_ext; ?>"><?php echo msg('NEXT_BIRTHDAYS'); ?></a>
 	</li>
     <li>
-    	<img src="<?php echo $url_images; ?>icons/printer.png" alt="print all" />
     	<a href="view<?php echo $page_ext_qry; ?>all&amp;print"><?php echo msg('PRINT_ALL'); ?></a>
   </li>
     <li>
-      <img src="<?php echo $url_images; ?>icons/book_open.png" alt="print phones" />
       <a href="view<?php echo $page_ext_qry; ?>all&amp;print&amp;phones"><?php echo msg('PRINT_PHONES'); ?></a>
   </li>
     <li>
-      <img src="<?php echo $url_images; ?>icons/page_excel.png" alt="export csv" />
     	<a href="csv<?php echo $page_ext; ?>"><?php echo msg('EXPORT_CSV'); ?></a>
   </li> 
 </ul>
