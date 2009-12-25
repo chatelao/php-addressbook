70057f13b7701e7246960d2a439b11c60af282a6
chatelao <chatelao@93e2e7f1-d98a-4af9-bc02-d7c9e78ee626>
2009-12-25 11:08:25 +0000

    Chg: Access to DB. Add: JSON container for AJAX-Reload.
    git-svn-id: https://svn.code.sf.net/p/php-addressbook/code/trunk@279 93e2e7f1-d98a-4af9-bc02-d7c9e78ee626

--- include/address.class.php
+++ include/address.class.php
@@ -52,4 +52,57 @@ class Address {
                          str_replace(".", "", $this->firstPhone()))));
     }
 }
+
+class Addresses {
+	  	
+    private $result;
+
+    function __construct($searchstring) {
+    	
+	    global $base_from_where, $table;
+
+     	$sql = "SELECT DISTINCT $table.* FROM $base_from_where";
+        
+      if ($searchstring) {
+        
+          $searchwords = split(" ", $searchstring);
+        
+          foreach($searchwords as $searchword) {
+          	$sql .= "AND (   lastname  LIKE '%$searchword%' 
+                          OR firstname LIKE '%$searchword%' 
+                          OR company   LIKE '%$searchword%' 
+                          OR address   LIKE '%$searchword%' 
+                          OR home      LIKE '%$searchword%'
+                          OR mobile    LIKE '%$searchword%'
+                          OR work      LIKE '%$searchword%'
+                          OR email     LIKE '%$searchword%'
+                          OR email2    LIKE '%$searchword%'
+                          OR address2  LIKE '%$searchword%' 
+                          OR notes     LIKE '%$searchword%' 
+                          )";
+          }          
+        }
+        
+        if(true) {
+            $sql .= "ORDER BY lastname, firstname ASC";
+        } else {
+          	$sql .= "ORDER BY firstname, lastname ASC";
+        }
+      	$this->result = mysql_query($sql);
+    }
+    
+    public function nextAddress() {
+    	
+    	$myrow = mysql_fetch_array($this->result);
+    	if($myrow) {
+		      return new Address($myrow);
+		  } else {
+		      return false;
+		  }		  
+    }
+
+    public function getResults() {
+    	return $this->result;
+    }
+}
 ?>
