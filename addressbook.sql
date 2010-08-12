e8b760f85daf8484c8a1f33a07c9e0d3258fa276
chatelao <chatelao@93e2e7f1-d98a-4af9-bc02-d7c9e78ee626>
2010-08-12 21:17:57 +0000

    Chg: Move views to utilities. Chg: Show "map" only, if a google key is avaliable.
    git-svn-id: https://svn.code.sf.net/p/php-addressbook/code/trunk@387 93e2e7f1-d98a-4af9-bc02-d7c9e78ee626

--- addressbook.sql
+++ addressbook.sql
@@ -88,14 +88,3 @@ CREATE TABLE user_prefs (
   PRIMARY KEY  (`id`,`pref_key`),
   KEY `fk_id` (`id`)
 ) DEFAULT CHARSET=utf8;
-
-CREATE VIEW vw_address AS select * from addressbook where isnull(deprecated);
-
-CREATE VIEW vw_address_deleted AS
-select * from addressbook
-where (id,deprecated) in (select max(id), deprecated from addressbook 
- where deprecated > 0 group by id)
-  and (id) not in (select id from addressbook 
- where deprecated is null
-group by id)
-order by deprecated desc;
