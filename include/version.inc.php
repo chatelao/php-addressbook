b21ffc3db8c7904114d5d87a0268bca2c275de71
chatelao <chatelao@93e2e7f1-d98a-4af9-bc02-d7c9e78ee626>
2012-10-04 07:51:57 +0000

    - Upd: Version to 8.1.15
    git-svn-id: https://svn.code.sf.net/p/php-addressbook/code/trunk@536 93e2e7f1-d98a-4af9-bc02-d7c9e78ee626

--- include/version.inc.php
+++ include/version.inc.php
@@ -8,5 +8,5 @@
 
 $revision = '$Rev$';
 $revision = str_replace('$', '', str_replace(' ', '', str_replace('Rev: ', '', $revision)));
-$version = '8.1.13'.' - r'.$revision;
+$version = '8.1.15'.' - r'.$revision;
 ?>
