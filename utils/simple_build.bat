8b14d90faf311b90071eb45bd761ade629d6515f
chatelao <chatelao@users.sourceforge.net>
2012-10-08 14:59:57 +0200

    - Chg: Build from github.com

--- utils/simple_build.bat
+++ utils/simple_build.bat
@@ -1,14 +1,18 @@
 
 
 set abs_zips=C:\Users\BLACKY\Desktop\all-in-one\addressbook
-set abs_temp=%abs_zipsabs_zips%\addressbook
+set abs_temp=%abs_zips%\addressbook
 
 set /P version="Enter the version: "
 echo %version%
 
-rmdir %abs_temp%
+rmdir %abs_zips% /s /q
+mkdir %abs_zips%
 
-svn export svn://svn.code.sf.net/p/php-addressbook/code/trunk %abs_temp%
+REM svn export svn://svn.code.sf.net/p/php-addressbook/code/trunk %abs_temp%
+svn export http://svn.github.com/chatelao/php-addressbook.git %abs_temp%
+
+pause
 
 cscript simple_build_zip.vbs %abs_temp%\hybridauth %abs_temp%\hybridauth.zip
 rmdir %abs_temp%\hybridauth /s /q
