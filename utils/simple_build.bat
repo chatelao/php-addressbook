5d988dacf48690c8b768ff7147087a5501d925f7
chatelao <chatelao@users.sourceforge.net>
2012-10-08 16:49:51 +0200

    - Fix: Add empty logs and states directory to z-push.

--- utils/simple_build.bat
+++ utils/simple_build.bat
@@ -12,16 +12,15 @@ mkdir %abs_zips%
 REM svn export svn://svn.code.sf.net/p/php-addressbook/code/trunk %abs_temp%
 svn export http://svn.github.com/chatelao/php-addressbook.git %abs_temp%
 
-pause
-
 cscript simple_build_zip.vbs %abs_temp%\hybridauth %abs_temp%\hybridauth.zip
-rmdir %abs_temp%\hybridauth /s /q
-
 cscript simple_build_zip.vbs %abs_temp%\z-push     %abs_temp%\z-push.zip
-rmdir %abs_temp%\z-push /s /q
 
 rmdir %abs_temp%\jscalendar /s /q
-rmdir %abs_temp%\test /s /q
+rmdir %abs_temp%\utils      /s /q
+rmdir %abs_temp%\test       /s /q
+
+rmdir %abs_temp%\hybridauth /s /q
+rmdir %abs_temp%\z-push /s /q
 
 del %abs_temp%v%verson%.zip
 cscript simple_build_zip.vbs %abs_temp%            %abs_zips%v%version%.zip
