440b2596e98dd1bbdabcadefc2bbbda2a21e189b
chatelao <chatelao@users.sourceforge.net>
2012-10-15 22:53:19 +0200

    - Chg: Inlude git keywords instead of svn.

--- include/version.inc.php
+++ include/version.inc.php
@@ -6,7 +6,7 @@
  * - $Rev$
  */
 
-$revision = '$Rev$';
+$revision = '$Format:%H$';
 $revision = str_replace('$', '', str_replace(' ', '', str_replace('Rev: ', '', $revision)));
 $version = '8.1.15'.' - r'.$revision.'@github';
 ?>
