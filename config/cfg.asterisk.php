2710b9a539ab0943668ff300495f077eba3417f4
bss <info@bss.si>
2013-02-17 18:31:21 +0100

    Added config file for asterisk integration.

---
+++ config/cfg.asterisk.php
@@ -0,0 +1,9 @@
+<?php
+
+// Asterisk AMI manager for click to call
+
+$asterisk_ami_host = 'localhost';
+$asterisk_ami_port = 5038;
+$asterisk_ami_user = 'admin';
+$asterisk_ami_pass = 'amipassword';
+
