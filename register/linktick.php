98d7775a9ca3c1336ba32343e5e067249e5a40e7
chatelao <2461532+chatelao@users.noreply.github.com>
2026-04-28 10:39:48 +0000

    Migrate registration module (Part 1) to DBAL
    Migrated the following files to use the Database Abstraction Layer (DBAL) with prepared statements:
    - register/linktick.php
    - register/delete_user.php
    - register/email_sent.php
    - register/edit_user_save.php
    
    Additional improvements:
    - Replaced legacy short open tags (`<?`) with full PHP tags (`<?php`) for modern PHP compatibility.
    - Added `isset()` checks for `$_REQUEST`, `$_COOKIE`, and `$_POST` parameters to prevent "Undefined index" notices.
    - Fixed syntax errors (missing semicolons and mismatched braces) in `register/email_sent.php`.
    - Updated `MIGRATION_ROADMAP.md` to reflect the completed tasks.

--- register/linktick.php
+++ register/linktick.php
@@ -1,41 +1,44 @@
-<?php
-
-
-include"master_inc.php";
-
-
-function sanitize_paranoid_string($string, $min='', $max='')
-{
-  $string = preg_replace("/[^a-zA-Z0-9:\/\._]/", "", $string);
-  $len = strlen($string);
-  if((($min != '') && ($len < $min)) || (($max != '') && ($len > $max)))
-    return FALSE;
-  return $string;
-}
-	  
-	  
-	       $site = $_REQUEST['site'];
-		   $site = sanitize_paranoid_string($site);		
-		   
-		   //Time & Date
-			$date = date ('m/d/y  g:i a');
-			
-			//IP Address
-			$ip = $_SERVER['REMOTE_ADDR'];
-			
-			$type = "link";
-			
-	   $query = "INSERT INTO `traffic` (`date`,`ip`,`link`,`notes`,`site`,`type`) 
-         VALUES ( '$date','$ip','$link','$notes','$site','$type')";   
-		 		 
-		  // save the info to the database
-   $results = mysql_query( $query );
-
-$url = str_replace("316AMPERSAND316","&","$link");
-
-//echo "URL: $url";
-
-header ("Location: $url");
-
-
-?>
+<?php
+
+
+include"master_inc.php";
+
+
+function sanitize_paranoid_string($string, $min='', $max='')
+{
+  $string = preg_replace("/[^a-zA-Z0-9:\/\._]/", "", $string);
+  $len = strlen($string);
+  if((($min != '') && ($len < $min)) || (($max != '') && ($len > $max)))
+    return FALSE;
+  return $string;
+}
+
+
+	       $site = isset($_REQUEST['site']) ? $_REQUEST['site'] : "";
+		   $site = sanitize_paranoid_string($site);
+
+		   $link = isset($_REQUEST['link']) ? $_REQUEST['link'] : "";
+		   $notes = isset($_REQUEST['notes']) ? $_REQUEST['notes'] : "";
+
+		   //Time & Date
+			$date = date ('m/d/y  g:i a');
+
+			//IP Address
+			$ip = $_SERVER['REMOTE_ADDR'];
+
+			$type = "link";
+
+	   $query = "INSERT INTO `traffic` (`date`,`ip`,`link`,`notes`,`site`,`type`)
+         VALUES ( ?, ?, ?, ?, ?, ?)";
+
+		  // save the info to the database
+   $results = $db_access->execute( $query, array($date, $ip, $link, $notes, $site, $type) );
+
+$url = str_replace("316AMPERSAND316","&",$link);
+
+//echo "URL: $url";
+
+header ("Location: $url");
+
+
+?>
